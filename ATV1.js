3aa1a6063f94b3576a5d8f98831ddaa0498ee828
CamisNextage <0001271707@senaimgaluno.com.br>
2026-06-24 14:58:27 -0300

    Add files via upload

---
+++ ATV1.js
@@ -0,0 +1,3 @@
+let QuilometrosPorHora= Number(prompt("Digite o número que deseja converter :"));
+let ResultadoMetros= QuilometrosPorHora * 3.6;
+alert(QuilometrosPorHora + " O quilometro por hora hm/h, equivalem a  " + ResultadoMetros + " metros! ");
